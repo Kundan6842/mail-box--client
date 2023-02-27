8ac4ddbdadade4f794a3cc2de7bbd38c9da7328d
Kundan <kundanmahato34@gmail.com>
2023-02-26 19:46:11 -0800

    added authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import AuthForm from "./Components/Authentication/AuthForm";
+
+import Homepage from "./Components/Homepage/Homepage";
+
+import { useSelector } from "react-redux";
 
 function App() {
+  const IsLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!IsLoggedIn && <AuthForm />}
+      {IsLoggedIn && <Homepage />}
     </div>
   );
 }
---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+      hi welcome to mail box client
+    </div>
+  )
+}
+
+export default Homepage
